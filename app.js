e0c40b9e70dd1a9561ca7c4f2df4a2a393c2086d
arc4dev <sroczyk.arkadiusz@gmail.com>
2023-10-04 13:53:41 +0200

    init

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
 
-const contactsRouter = require('./routes/api/contacts')
+const app = express();
 
-const app = express()
+// Middlewares
+app.use(morgan(app.get('env') === 'development' ? 'dev' : 'short'));
+app.use(cors());
+app.use(express.json());
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+// Routes
+// app.use('/api/contacts', someRoute);
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+// Handle not defined routes
+app.all('*', (req, res, next) => {
+  res.status(404).json({ message: 'Route not found!' });
+});
+app.use((req, res) => {});
 
-app.use('/api/contacts', contactsRouter)
-
-app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
-
+// Error Handler
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
-})
+  res.status(500).json({ message: err.message });
+});
 
-module.exports = app
+module.exports = app;
--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-const app = require('./app')
+// Handling uncaught exceptions
+process.on('uncaughtException', err => {
+  console.log(err.name, err.message, err.stack);
+  process.exit(1);
+});
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require('./app');
+
+const PORT = process.env.PORT | 3000;
+const server = app.listen(PORT, () => {
+  console.log(`Server running. Use our API on port: ${PORT}`);
+});
+
+// Handling promise rejections
+process.on('unhandledRejection', err => {
+  console.log(err.name, err.message);
+  server.close(() => process.exit(1));
+});
