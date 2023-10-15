226dda71d24300433e458765439f6e94ed949d18
arc4dev <sroczyk.arkadiusz@gmail.com>
2023-10-15 12:09:29 +0200

    fix port operation

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ mongoose
   .catch(() => process.exit(1));
 
 // Server launching
-const PORT = process.env.PORT | 3001;
+const PORT = process.env.PORT || 3001;
 const server = app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
 });
